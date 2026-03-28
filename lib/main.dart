106277f0e9a57d976d6eb4b14cd6e828999b635f
Muhammad Hassan <iam.muhammad.hassan0@gmail.com>
2026-03-28 11:56:59 -0700

    AppBar Completed

--- lib/main.dart
+++ lib/main.dart
@@ -14,16 +14,34 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(primarySwatch: Colors.blue),
       home: Scaffold(
         appBar: AppBar(
+          
+          toolbarHeight: 80,
           backgroundColor: Color.fromARGB(255, 4, 104, 34),
           title: Column(
             children: [
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Text('Whatsapp'),
-                  Row(children: [Icon(Icons.search), Icon(Icons.more_vert)]),
+                  Text('Whatsapp', style: TextStyle(color:Colors.white, fontWeight: FontWeight.bold)),
+                  Row(children: [Icon(Icons.search,color:Colors.white,), Icon(Icons.more_vert,color: Colors.white,)]),
                 ],
               ),
+              SizedBox(height: 15),
+              
+              Row(
+                
+              
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                
+                children: [
+                  
+                  Icon( Icons.camera_alt_outlined, color: Colors.white,),
+        
+                  Text('CHATS', style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white )),
+                  Text('STATUS', style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white )),
+                  Text('CALLS', style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white )),
+                ],
+              )
             ],
           ),
         ),
