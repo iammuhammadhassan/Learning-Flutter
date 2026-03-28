a1204dabcea3aefb009e3f6a872a353086aa721f
Muhammad Hassan <iam.muhammad.hassan0@gmail.com>
2026-03-28 12:16:48 -0700

    Added floating icon

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,6 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(primarySwatch: Colors.blue),
       home: Scaffold(
         appBar: AppBar(
-          
           toolbarHeight: 80,
           backgroundColor: Color.fromARGB(255, 4, 104, 34),
           title: Column(
@@ -22,34 +21,74 @@ class MyApp extends StatelessWidget {
               Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Text('Whatsapp', style: TextStyle(color:Colors.white, fontWeight: FontWeight.bold)),
-                  Row(children: [Icon(Icons.search,color:Colors.white,), Icon(Icons.more_vert,color: Colors.white,)]),
+                  Text(
+                    'Whatsapp',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      Icon(Icons.search, color: Colors.white),
+                      Icon(Icons.more_vert, color: Colors.white),
+                    ],
+                  ),
                 ],
               ),
               SizedBox(height: 15),
-              
+
               Row(
-                
-              
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                
+
                 children: [
-                  
-                  Icon( Icons.camera_alt_outlined, color: Colors.white,),
-        
-                  Text('CHATS', style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white )),
-                  Text('STATUS', style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white )),
-                  Text('CALLS', style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white )),
+                  Icon(Icons.camera_alt_outlined, color: Colors.white),
+
+                  Text(
+                    'CHATS',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Text(
+                    'STATUS',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Text(
+                    'CALLS',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
                 ],
-              )
+              ),
             ],
           ),
         ),
 
         backgroundColor: Color.fromARGB(255, 255, 255, 255),
 
-        body: Center(child: Text('Hello, World!')),
+        body:  Center(child: Text("Hello")),
+
+        floatingActionButton: SizedBox(
+          height: 70,
+          width: 70,
+          
+          child: FloatingActionButton(
+            onPressed: () {
+              print("Button clicked");
+            },
+            backgroundColor: Color.fromARGB(255, 4, 104, 34),
+            child:  Icon(Icons.message,
+            color: Colors.white,
+          ),
+        ),
       ),
-    );
+    ));
   }
 }
